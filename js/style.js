fa8f80fbb3cac17b209c26f5e31e3626dd92f324
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2020-06-03 12:50:55 +0600

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#skills").mousemove(function(){
+    $('#bar1').barfiller({duration: 1800, barColor:"#F5633C"});
+     $('#bar2').barfiller({duration: 1600, barColor:"#23B2D2"});
+     $('#bar3').barfiller({duration: 1000, barColor:"#E1F519"});
+     $('#bar4').barfiller({duration: 1300, barColor:"#217CA7"});
+     $('#bar5').barfiller({duration: 1400, barColor:"#4F5576"});
+     $('#bar6').barfiller({duration: 1600, barColor:"#009CCC"});
+     $('#bar7').barfiller({duration: 1600, barColor:"#182FAF"});
+    });
+    $("#skillBtn").click(function(){
+     $('#bar1').barfiller({duration: 1800, barColor:"#F5633C"});
+     $('#bar2').barfiller({duration: 1600, barColor:"#23B2D2"});
+     $('#bar3').barfiller({duration: 1000, barColor:"#E1F519"});
+     $('#bar4').barfiller({duration: 1300, barColor:"#217CA7"});
+     $('#bar5').barfiller({duration: 1400, barColor:"#4F5576"});
+     $('#bar6').barfiller({duration: 1600, barColor:"#009CCC"});
+     $('#bar7').barfiller({duration: 1600, barColor:"#182FAF"});
+    });
+     });
+       
+    
